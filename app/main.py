832bb812497fa90f626270d82d41ef9e6c9a2703
KK <157890059+KostinKKKK@users.noreply.github.com>
2026-03-31 10:18:16 +0300

    Fix of docker + origins added

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # main endpoint for this API
 my_name = "Sergei"
 
+origins = [
+    "http://127.0.0.1:5500",
+    "https://gg-team-repo-git-web-communication-and-databases-1.2.rahtiapp.fi",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/", response_class=HTMLResponse)
+
+
+@app.get("/api/ip", response_class=HTMLResponse)
 def read_root(request: Request):
     my_IP = request.client.host
     return f"<h1>Hello, your ip is {my_IP}</h1>"
