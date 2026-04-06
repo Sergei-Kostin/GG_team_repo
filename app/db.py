6f4200a1312c4eab1a3d58e53e2ff6b6dd11bab8
KK <157890059+KostinKKKK@users.noreply.github.com>
2026-04-06 13:33:31 +0300

    Added hotel db schema

--- app/db.py
+++ app/db.py
@@ -8,10 +8,30 @@ def get_conn():
 def create_schema():
     with get_conn() as conn, conn.cursor() as cur:
         cur.execute("""
-            CREATE TABLE IF NOT EXISTS rooms (
+            CREATE TABLE IF NOT EXISTS hotel_rooms (
                 id SERIAL PRIMARY KEY,
                 room_number int NOT NULL,
+                room_type VARCHAR(50) NOT NULL,
+                price numeric(10, 2) NOT NULL,
                 created_at TIMESTAMP DEFAULT now()
             );
-            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS room_type VARCHAR(50);
+            CREATE TABLE IF NOT EXISTS hotel_guests (
+                id SERIAL PRIMARY KEY,
+                firstname VARCHAR(100) NOT NULL,
+                lastname VARCHAR(100) NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT now()
+            );
+            CREATE TABLE IF NOT EXISTS hotel_bookings (
+                id SERIAL PRIMARY KEY,
+                guest_id INT REFERENCES hotel_guests(id),
+                room_id INT REFERENCES hotel_rooms(id),
+                datefrom TIMESTAMP NOT NULL,
+                dateto TIMESTAMP NOT NULL,
+                addinfo VARCHAR(255),
+                created_at TIMESTAMP DEFAULT now()
+            );
         """)
+
+
+
