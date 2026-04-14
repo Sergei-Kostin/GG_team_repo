982c2ec1ee7932602cba0223e48104ece475dbf2
KK <157890059+KostinKKKK@users.noreply.github.com>
2026-04-14 15:58:16 +0300

    mm

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import get_conn
 from app.db import create_schema
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
 app = FastAPI()
 
 # main endpoint for this API
@@ -29,6 +33,14 @@ app.add_middleware(
 
 create_schema()
 
+class Booking(BaseModel):
+    guest_id: int
+    room_id: int
+    datefrom: Optional[date] = None
+    dateto: Optional[date] = None
+    addinfo: Optional[str] = None
+
+
 @app.get("/api/rooms")
 def get_rooms():
     with get_conn() as conn, conn.cursor() as cur:
@@ -36,9 +48,37 @@ def get_rooms():
         rows = cur.fetchall()
         return {"hotel_rooms": rows}
 
+@app.get("/api/rooms/id/{id}")
+def get_room_by_id(id: int):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms WHERE id = %s", (id,))
+        row = cur.fetchone()
+        return {"hotel_room": row}
+
 @app.post("/api/booking")
-def create_booking():
-    return {"msg": "Booking created"}
+def create_booking(booking: Booking):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+                    INSERT INTO hotel_bookings 
+                    (guest_id, room_id, datefrom, dateto, addinfo) 
+                    VALUES (%s, %s, COALESCE(%s, now()), COALESCE(%s, now()), %s)
+                    RETURNING id
+                    """, 
+                    (booking.guest_id, booking.room_id, booking.datefrom, booking.dateto, booking.addinfo)
+                    )
+        new_booking_id = cur.fetchone()["id"]
+        conn.commit()
+        return {"msg": "Booking created successfully",
+                "id": new_booking_id,
+                "room_id": booking.room_id
+                }
+
+@app.get("/api/bookings")
+def get_bookings():
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_bookings")
+        rows = cur.fetchall()
+        return {"hotel_bookings": rows}
 
 @app.get("/api/ip", response_class=HTMLResponse)
 def read_root(request: Request):
